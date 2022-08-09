83ee41670a1ae18f82713a7a210b8a4da077494c
Francisco Cortez <franciscocortezdev@gmail.com>
2022-08-08 19:32:13 -0600

    funcionalidad del filtro btns hecho

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { useState } from 'react'
+import React, { useState } from 'react'
 import { people, calendar, emails } from './Data'
 
 function App () {
@@ -7,9 +7,43 @@ function App () {
   const [selection, setSelection] = useState(null)
   const [currentOption, setCurrentOption] = useState('all')
 
+  const handleClick = (e:React.FormEvent<HTMLButtonElement>) => {
+    const { name } = e.currentTarget
+
+    switch (name) {
+      case 'all':
+        setData([...people, ...calendar, ...emails])
+        setCurrentOption('all')
+        break
+      case 'people':
+        setData([...people])
+        setCurrentOption('people')
+        break
+
+      case 'calendar':
+        setData([...calendar])
+        setCurrentOption('calendar')
+        break
+
+      case 'email':
+        setData([...emails])
+        setCurrentOption('email')
+        break
+
+      default:
+        break
+    }
+  }
   return (
-    <div className="App">
-     <h1>Hello World</h1>
+    <div>
+     <button onClick={handleClick} name='all'>All</button>
+     <button onClick={handleClick} name='people'>People</button>
+     <button onClick={handleClick} name='calendar'>Calendar</button>
+     <button onClick={handleClick} name='email'>Emails</button>
+
+     <div>
+        {data.map((item) => (<div key={item.id}>{item.title}</div>))}
+     </div>
     </div>
   )
 }
