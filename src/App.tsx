422086b8805f74caa2e6ecfa0a7f8223b237f4b4
Francisco Cortez <franciscocortezdev@gmail.com>
2022-08-08 18:00:18 -0600

    Data People added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,28 @@
 
 import './App.css';
 
+const people = [
+  {
+    id: 'people-01',
+    title: 'Juan Perez'
+  },
+  {
+    id: 'people-02',
+    title: 'Marcos Rivas'
+  },
+  {
+    id: 'people-03',
+    title: 'Sergio Martinez'
+  },
+  {
+    id: 'people-04',
+    title: 'Laura Jimenez'
+  },
+  {
+    id: 'people-05',
+    title: 'Horario Martinez'
+  }
+]
 function App() {
   return (
     <div className="App">
