26ea228bfe545faede2f19577df42067b39195fd
Francisco Cortez <franciscocortezdev@gmail.com>
2022-08-08 19:20:01 -0600

    eslint config added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import './App.css';
-import { people, calendar, emails } from './Data';
+import './App.css'
+import { useState } from 'react'
+import { people, calendar, emails } from './Data'
 
-function App() {
+function App () {
+  const [data, setData] = useState([...people, ...calendar, ...emails])
+  const [selection, setSelection] = useState(null)
+  const [currentOption, setCurrentOption] = useState('all')
 
- 
   return (
     <div className="App">
      <h1>Hello World</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
