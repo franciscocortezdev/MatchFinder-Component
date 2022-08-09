e150e978eef1374c959e279e48f781bb7afbf50c
Francisco Cortez <franciscocortezdev@gmail.com>
2022-08-08 18:10:20 -0600

    Data added

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,9 @@
-
 import './App.css';
+import { people, calendar, emails } from './Data';
 
-const people = [
-  {
-    id: 'people-01',
-    title: 'Juan Perez'
-  },
-  {
-    id: 'people-02',
-    title: 'Marcos Rivas'
-  },
-  {
-    id: 'people-03',
-    title: 'Sergio Martinez'
-  },
-  {
-    id: 'people-04',
-    title: 'Laura Jimenez'
-  },
-  {
-    id: 'people-05',
-    title: 'Horario Martinez'
-  }
-]
-
-const calendar = [
-  {
-    id: 'calendar-01',
-    title: 'Sesion de seguimiento'
-  },
-  {
-    id: 'calendar-02',
-    title: 'Revision de propuestas'
-  },
-  {
-    id: 'calendar-03',
-    title: 'Evento para donar juguetes'
-  },
-  {
-    id: 'calendar-04',
-    title: 'Junta semanal de equipo'
-  },
-  {
-    id: 'calendar-05',
-    title: 'Revision de pendientes con cliente'
-  }
-]
 function App() {
+
+ 
   return (
     <div className="App">
      <h1>Hello World</h1>
---
+++ src/Data.ts
@@ -0,0 +1,68 @@
+export const people = [
+  {
+    id: 'people-01',
+    title: 'Juan Perez'
+  },
+  {
+    id: 'people-02',
+    title: 'Marcos Rivas'
+  },
+  {
+    id: 'people-03',
+    title: 'Sergio Martinez'
+  },
+  {
+    id: 'people-04',
+    title: 'Laura Jimenez'
+  },
+  {
+    id: 'people-05',
+    title: 'Horario Martinez'
+  }
+]
+
+export const calendar = [
+  {
+    id: 'calendar-01',
+    title: 'Sesion de seguimiento'
+  },
+  {
+    id: 'calendar-02',
+    title: 'Revision de propuestas'
+  },
+  {
+    id: 'calendar-03',
+    title: 'Evento para donar juguetes'
+  },
+  {
+    id: 'calendar-04',
+    title: 'Junta semanal de equipo'
+  },
+  {
+    id: 'calendar-05',
+    title: 'Revision de pendientes con cliente'
+  }
+]
+
+export const emails = [
+  {
+    id: 'email-01',
+    title: 'Reporte de resultados'
+  },
+  {
+    id: 'email-02',
+    title: 'Requisitos para cambio'
+  },
+  {
+    id: 'email-03',
+    title: 'Estatus de funcionalidad'
+  },
+  {
+    id: 'email-04',
+    title: 'Proximos eventos'
+  },
+  {
+    id: 'email-05',
+    title: 'Participa en la encuesta'
+  }
+]
